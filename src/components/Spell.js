ea8c131385a29f71003bc6933c10b1b9162a8da8
isaacdigs <pk07057@gmail.com>
2019-05-15 22:11:22 +0900

    removed Howling Abyss Summoner Spells

--- src/components/Spell.js
+++ src/components/Spell.js
@@ -4,17 +4,17 @@ export class Spell extends Component {
   
   render() {
     let spells = [
-      "Barrier", "Boost", "Dot", "Exhaust", "Flash", "Haste", "Heal", "Mana", "Snowball", "Teleport"
+      "Barrier", "Boost", "Dot", "Exhaust", "Flash", "Haste", "Heal", "Teleport"
     ];
-    let rng1n1 = Math.floor(Math.random() * 10);
-    let rng1n2 = Math.floor(Math.random() * 10);
+    let rng1n1 = Math.floor(Math.random() * 8);
+    let rng1n2 = Math.floor(Math.random() * 8);
     if (rng1n2 === rng1n1) {
-      rng1n2 = Math.abs(rng1n1 + (Math.floor(Math.random()*9))-10);
+      rng1n2 = Math.abs(rng1n1 + (Math.floor(Math.random()*7))-8);
     }
-    let rng2n1 = Math.floor(Math.random() * 10);
-    let rng2n2 = Math.floor(Math.random() * 10);
+    let rng2n1 = Math.floor(Math.random() * 8);
+    let rng2n2 = Math.floor(Math.random() * 8);
     if (rng2n2 === rng2n1) {
-      rng2n2 = Math.abs(rng2n1 + (Math.floor(Math.random()*9))-10);
+      rng2n2 = Math.abs(rng2n1 + (Math.floor(Math.random()*7))-8);
     }
 
     return (
